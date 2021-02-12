ba2437fd5550fcd1457d6336b00e2d3b864be3f2
tqpk <never@caughtlack.in>
2021-02-12 14:45:49 +0000

    vue application that takes username and password, sends http post with axios and then displays the returned data (user login token) from netsoc server

---
+++ sweng-group-41/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <TestButton></TestButton>
+  </div>
+</template>
+
+<script>
+import TestButton from './components/TestButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
