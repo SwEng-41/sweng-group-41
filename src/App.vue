cfb9ee156761a81175dc63629a5531be4fc5d1ca
tqpk <never@caughtlack.in>
2021-02-28 10:23:01 +0000

    added router module

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <div id="app">
-   <TestButton></TestButton>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
-
-<script>
-import TestButton from './components/TestButton.vue'
-
-export default {
-  name: 'App',
-  components: {
-    TestButton
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
