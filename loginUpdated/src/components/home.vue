1a83e6644ee90cdc0d41f41b63fb492654a46b2b
8n76nn98 <1945402109@qq.com>
2021-03-01 22:25:22 +0000

    new

---
+++ loginUpdated/src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h3>This is login page </h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
