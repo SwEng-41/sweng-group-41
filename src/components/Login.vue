dbedf1a0bedd0ad717bdaa859ffaece15f5ceff1
CaoilfhionnNiD <78646276+CaoilfhionnNiD@users.noreply.github.com>
2021-03-19 13:23:41 +0000

    Added forgot password modal and used css style

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,47 +1,91 @@
 <template>
-  <div id="login">
-    <h1>Login</h1>
-    <div v-if="requestSuccessful">
-      Post Request Created
+  <body>
+    <div id="login" class="login-box">     
+      <div v-if="requestSuccessful">
+        Check your email to reset your password
+      </div>
+      <h1>Login</h1>
+      <form>
+        <div class="user-box">
+          <input type="username" placeholder="Username" v-model="username" />
+          <input type="password" placeholder="Password" v-model="password" />
+        </div>
+        <a href="#" v-on:click="createPost"
+          >Login
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
+        <a href="#" @click="showModal = true">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Forgot Password?</a
+        >
+        <transition name="slide">
+          <div class="modal" v-if="showModal">
+              <h1>Forgot Password</h1>
+              <div class="user-box">
+                <input
+                  type="username"
+                  placeholder="Username"
+                  v-model="forgotUsername"
+                />
+              </div>
+              <a href="#" v-on:click="createPut" @click="showModal = false">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Reset Passsword
+              </a>
+            </div>
+        </transition>
+      </form>
     </div>
-    <input type="username" placeholder="Username" v-model="username" />
-    <br />
-    <br />
-    <input type="password" placeholder="Password" v-model="password" />
-    <br />
-    <br />
-    <button v-on:click="createPost">Login</button>
-    <p>
-      <router-link to="forgot">Forgot password?</router-link>
-    </p>
-  </div>
+  </body>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: 'Login',
+  name: "Login",
+   el: '#login',
   data() {
     return {
       username: "",
       password: "",
+      forgotUsername: "",
       requestSuccessful: false,
+      showModal: false,
     };
   },
   methods: {
     createPost() {
-        axios
-          .post("https://iam.netsoc.ie/v1/users/" + this.username + "/login", {
-            password: this.password,
-          })
-          .then((response) => {
-            this.requestSuccessful = true;
-            alert(response.data.token);
-          });
+      axios
+        .post("https://iam.netsoc.ie/v1/users/" + this.username + "/login", {
+          password: "",
+        });
+    },
+    async createPut() {
+      axios.put(
+        "https://iam.netsoc.ie/v1/users/" + this.forgotUsername + "/login",
+        {},
+        { headers: { Accept: "text/html" } }
+      ).then(()=> {this.requestSuccessful = true;});
     },
   },
 };
 </script>
 
 <style scoped>
+.slide-leave-active {
+ transition: transform 1s;
+}
+
+.slide-leave-to {
+ transform: translateY(-50%) translateX(100vw);
+}
 </style>
