ac8b129686270ad20105fbc2b6b988c640dcff1c
Eligijus Skersonas <eskersonas2000@gmail.com>
2021-03-22 17:42:24 +0000

    Added Modals to handle respone codes

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,205 +1,135 @@
 <template class="container main">
-  <!-- MAIN START -->
+    <!-- MAIN START -->
 
-  <div class="container registrationForm">
-    <!-- HEAD -->
-    <header class="head">
-      <h1>Register</h1>
-      <p>Please fill in this form to create an account.</p>
-      <hr />
-    </header>
-    <!-- HEAD END -->
+    <div class="container registrationForm">
+        <!-- HEAD -->
+        <header class="head">
+            <h1>Register</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr>
+        </header>
+        <!-- HEAD END -->
 
-    <!-- INPUT BOXES -->
-    <input v-model="username" placeholder="username" required />
-    <input v-model="firstname" placeholder="firstname" required />
-    <input v-model="lastname" placeholder="lastname" required />
-    <input v-model="email" placeholder="email" required />
-    <input v-model="repeatemail" placeholder="repeat email" required />
-    <input v-model="password" placeholder="password" required />
-    <input v-model="repeatpassword" placeholder="repeat password" required />
-    <!-- INPUT BOXES END -->
+        <!-- INPUT BOXES -->
+        <input v-model="username" placeholder="username" required>
+        <input v-model="firstname" placeholder="firstname" required>
+        <input v-model="lastname" placeholder="lastname" required>
+        <input v-model="email" placeholder="email" required>
+        <input v-model="repeatemail" placeholder="repeat email" required>
+        <input v-model="password" placeholder="password" required>
+        <input v-model="repeatpassword" placeholder="repeat password" required>
+        <!-- INPUT BOXES END -->
 
-    <!-- BUTTON -->
-    <button
-      v-on:click="onClick(username, firstname, lastname, email, password)"
-    >
-      Register
-    </button>
-    <!-- BUTTON END -->
+        <!-- BUTTON -->
+        <button 
+        class="btn-reg"
+        v-on:click="onClick(username, firstname, lastname, email, password)">
+        Register
+        </button>
 
-  <!-- MODAL DIALOG -->
-  <transition name="slide">
-    <div class="modal" v-if="showSuccess">
-      <h3>Your Account has been created! Please Check your email!</h3>
-      <a href="#" @click="showSuccess = false">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Close
-      </a>
+        <AccountCreatedModal
+        v-show="isAccountCreated"
+        @close="closeModal"
+        />
+        <SystemErrorModal
+        v-show="isServerIssue"
+        @close="closeModal"
+        />
+        <TakenModal
+        v-show="isTaken"
+        @close="closeModal"
+        />
+        <InputErrorModal
+        v-show="isInvalid"
+        @close="closeModal"
+        />
+        <MissingModal
+        v-show="isMissing"
+        @close="closeModal"
+        />
+        <!-- BUTTON END -->
+
+        <!-- SIGN IN -->
+        <div class="container SignIn">
+            <p>Already have an account? <a href="#">Sign in</a></p>
+        </div>
+        <!-- SIGN IN END -->
     </div>
-  </transition>
 
-  <transition name="slide">
-    <div class="modal" v-if="showError">
-      <h3>An Error has Occured</h3>
-      <a href="#" @click="showSuccess = false">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Close
-      </a>
-    </div>
-  </transition>
-  <!-- MODAL DIALOG END -->
-
-    <!-- SIGN IN -->
-    <div class="container SignIn">
-      <p>Already have an account? <a href="#">Sign in</a></p>
-    </div>
-    <!-- SIGN IN END -->
-
-  </div>
-
-
-
-  <!--
-    <div id="Logo">
-        <img src="../assets/logo.png" alt="Logo Image" height=200px width=200px>
-    </div>
-    -->
-
-  <!-- MAIN END -->
+    <!-- MAIN END -->
 </template>
 
 <script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      username: "",
-      password: "",
-      showSuccess: false,
-    };
-  },
-  methods: {
-    async onClick(username, firstname, lastname, email, password) {
-      try {
-        const res = await axios.post("https://iam.netsoc.ie/v1/users", {
-          username: username,
-          email: email,
-          password: password,
-          first_name: firstname,
-          last_name: lastname,
-        });
+ import axios from 'axios';
+ import AccountCreatedModal from './accountCreatedModal.vue';
+ import SystemErrorModal from './systemErrorModal.vue';
+ import TakenModal from './takenModal.vue';
+ import MissingModal from './missingModal.vue';
+ import InputErrorModal from './inputErrorModal.vue';
 
-        console.log(res);
 
-        if (res.status == 201) this.showSuccess = true;
-        else this.showError = true;
 
-      } catch (e) {
-        console.log(e);
-      }
-    },
-  },
-};
-</script>
+ export default {
+      components: {
+        AccountCreatedModal,
+        SystemErrorModal,
+        TakenModal,
+        MissingModal,
+        InputErrorModal,
+      },
 
-<style scoped>
-*,
-template,
-html,
-body {
-  margin: 0;
-  padding: 0;
-  border: none;
-  box-sizing: border-box;
-}
-/*
-    #Logo {
-        display: inline;
+      data() {
+        return {
+          isAccountCreated: false,
+          isServerIssue: false,
+          isTaken: false,
+          isMissing: false,
+          isInvalid: false,
+        };
+      },
+
+      methods: {
+         async onClick(username, firstname, lastname, email, password) {
+            try{
+            const res = await axios.post('https://iam.netsoc.ie/v1/users' ,
+                                        {
+                                            "username": username,
+                                            "email": email,
+                                            "password": password,
+                                            "first_name": firstname,
+                                            "last_name": lastname
+                                            }).catch(err => {
+                                              if(err.response.status == 409)
+                                                this.isTaken = true;
+                                              else if(err.response.status == 400)
+                                                this.isMissing = true;
+                                              else if(err.response.status == 401 || err.response.status == 403)
+                                                this.isInvalid = true;
+                                              else
+                                                this.isServerIssue = true;
+                                            }
+                                              );
+
+             console.log(res.data);
+
+             if(res.status == 201)
+              this.isAccountCreated = true;
+             
+            }catch(e){
+                console.log(e);
+            }
+         },
+
+      closeModal() {
+        this.isAccountCreated = false;
+        this.isTaken = false;
+        this.isServerIssue = false;
+        this.isInvalid = false;
+        this.isMissing = false;
+     },
+
     }
-    */
-.registrationForm {
-  outline: none;
-  display: inline;
-  float: right;
-  border-style: solid;
-  padding: 100px;
-  margin: 100px;
-  margin-top: 0px;
-  height: 100%;
-}
 
-input {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
+};
 
-input:focus {
-  background-color: #ddd;
-  outline-color: goldenrod;
-}
-
-/* Overwrite default styles of hr */
-hr {
-  border: 1px solid #ddd;
-  margin-bottom: 25px;
-}
-
-button {
-  background-color: goldenrod;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 50%;
-  opacity: 0.8;
-}
-
-button:hover {
-  opacity: 1;
-}
-
-a {
-  color: dodgerblue;
-  text-decoration: none;
-}
-
-.SignIn {
-  text-align: center;
-}
-
-.slide-leave-active {
- transition: transform 1s;
-}
-.slide-leave-to {
- transform: translateY(-50%) translateX(100vw);
-}
-
-.modal{
-    position: fixed;
-    top: 50%;
-    left: 150%;
-    transform: translate(-50%, -50%);
-    z-index: 150;
-    height: 50%;
-    opacity: 100%;
-
-    width: 70%;
-    max-width: 400px;
-    background: rgba(0, 0, 0, 0.5);
-    border-radius: 16px;
-    padding: 25px;
-  }
-
-</style>
+</script>
