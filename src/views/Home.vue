f7bce0ebf87646fd6219e1fc94dab8d265b5f477
swenginator <72451636+swenginator@users.noreply.github.com>
2021-03-31 17:49:58 +0100

    Add Netsoc logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/Netsoc-Logo.png"/>
-
-
-    <Button label="Darragh" @click="testButton()"></Button>
-    <SingleBoxForm label="Change Name"></SingleBoxForm>
-    <SingleBoxForm label="Change First Name"></SingleBoxForm>
-    <SingleBoxForm label="Change Last Name"></SingleBoxForm>
-
+  <div class="container">
+    <img alt="Netsoc logo" src="../assets/logo-bitmap.png" style="width:90%"/>
   </div>
 </template>
 
 <script>
-import Button from "@/components/Button.vue";
-import SingleBoxForm from '@/components/SingleBoxForm.vue';
 
 export default {
   name: 'Home',
-  components: {
-    Button,
-    SingleBoxForm
-  },
-
-  methods: {
-    testButton() {
-      alert("Button pressed!");
-    }
-  }
+  components: {},
+  methods: {}
 }
 </script>
