29fd6bfaf51a62c0eeb0a8da3c1f1aa5ad45d117
swenginator <72451636+swenginator@users.noreply.github.com>
2021-04-05 22:48:56 +0100

    Reconcile style & verification for login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,109 +1,53 @@
 <template>
+  <div class="box loginForm">
 
-<html>
-  <body>
-
-  <div class="box">
-
-    <h2>NetSoc Login</h2>
-
+    <h2>Login</h2>
     <div class="section">
 
-      <div class="inputbox">
-        <input type="text" id="username" name="" required="">
-        <label>Username</label>
-      </div>
-
-      <div class="inputbox">
-        <input type="password" id="password" name="" required="">
-        <label>Password</label>
-      </div>
-
-      <a v-on:click="login()" href="#" onclick="return false;">
-
-        <div class="button">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          login
+      <Form @submit="login" :validation-schema="formSchema">
+        <div class="inputbox">
+          <Field name="username" placeholder="Username"/>
+          <ErrorMessage class="vee-error" name="username"/>
         </div>
 
-      </a>
+        <div class="inputbox">
+          <Field name="password" placeholder="Password" type="password"/>
+          <ErrorMessage class="vee-error" name="password"/>
+        </div>
 
+        <button>
+          <div class="button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Login
+          </div>
+        </button>
+
+      </Form>
     </div>
 
     <div class="section">
-
-
       <a v-on:click="registrationPage()" href="#" onclick="return false;">
-
-        <div class="buttonnoanim">
-          register
-        </div>
-
+        <div class="buttonnoanim">register</div>
       </a>
 
       <a v-on:click="forgotPassword()" href="#" onclick="return false;">
-
-        <div class="buttonnoanim">
-          forgot password
-        </div>
-
+        <div class="buttonnoanim">forgot password</div>
       </a>
-
     </div>
 
   </div>
 
-  <Modal v-show="isInvalid" @close="closeModal" title="Incorrect Password" body="The password you entered is not correct!"/>
-  <Modal v-show="nonExistentUser" @close="closeModal" title="Non Existent User" body="A user with that username does not exist!"/>
-  <Modal v-show="isServerIssue" @close="closeModal" title="Server Issue" body="Unfortunately we are experiencing some issues. Please try again later!"/>
-  <Modal v-show="isLoginSuccessful" @close="closeModal" title="Login Successful" body="You logged in successfully!"/>
-
-  </body>
-</html>
-<!--
-  <div id="login" class="container loginForm">
-
-    <header class="head">
-      <h1>Login</h1>
-    </header>
-
-    <Form @submit="loginSubmit" :validation-schema="formSchema">
-      <Field name="username" placeholder="Username"/>
-      <ErrorMessage name="username"/>
-
-      <br/>
-      <br/>
-
-      <Field name="password" placeholder="Password" type="password"/>
-      <ErrorMessage name="password"/>
-
-      <br/>
-      <br/>
-
-      <button class="btn-reg">Login</button>
-
-    </Form>
-
-    <button class="btn-reg" v-on:click="registerPage">Register</button>
-      <br/>
-
-    <div class="container ForgotPassword">
-      <router-link to="forgot">Forgot password?</router-link>
-    </div>
-
-    <Modal v-show="isInvalid" @close="closeModal" title="Incorrect Password"
-           body="The password you entered is not correct!"/>
-    <Modal v-show="nonExistentUser" @close="closeModal" title="Non Existent User"
-           body="A user with that username does not exist!"/>
-    <Modal v-show="isServerIssue" @close="closeModal" title="Server Issue"
-           body="Unfortunately we are experiencing some issues. Please try again later!"/>
-    <Modal v-show="isLoginSuccessful" @close="closeModal" title="Login Successful" body="You logged in successfully!"/>
-
-  </div>
-  -->
+  <Modal v-show="isInvalid" @close="closeModal" title="Incorrect Password"
+         body="The password you entered is not correct!"/>
+  <Modal v-show="nonExistentUser" @close="closeModal" title="Non Existent User"
+         body="A user with that username does not exist!"/>
+  <Modal v-show="isServerIssue" @close="closeModal" title="Server Issue"
+         body="Unfortunately we are experiencing some issues. Please try again later!"/>
+  <Modal v-show="isLoginSuccessful" @close="closeModal" title="Login Successful"
+         body="You logged in successfully!"/>
 </template>
 
 <script>
@@ -114,10 +58,20 @@ import * as yup from "yup";
 
 export default {
   components: {
-    Modal,/*
+    Modal,
     Field,
     Form,
-    ErrorMessage,*/
+    ErrorMessage,
+  },
+
+  setup() {
+    const formSchema = yup.object({
+      username: yup.string().required().label("Username"),
+      password: yup.string().required().min(8).label("Password"),
+    });
+    return {
+      formSchema: formSchema,
+    };
   },
 
   name: "Login",
@@ -135,29 +89,24 @@ export default {
 
   methods: {
 
-    login() {
+    async login({username, password}) {
+      try {
+        const response = await axios.post('https://iam.netsoc.ie/v1/users/' + username + '/login', {"password": password})
+        await this.$router.push({name: 'Account', params: {jwt: response.data.token}});
 
-      let username = document.getElementById("username").value;
-      let password = document.getElementById("password").value;
-
-      axios.post('https://iam.netsoc.ie/v1/users/' + username + '/login', {"password": password})
-      .then(response => {
-        console.log("Successful Login!");
-        this.$router.push({ name: 'Account', params: { jwt : response.data.token } });
-      }).catch(error => {
-        if (error.response.status === 404) this.nonExistentUser = true;
-        else if (error.response.status === 401) this.isInvalid = true;
+      } catch (err) {
+        if (err.response.status === 404) this.nonExistentUser = true;
+        else if (err.response.status === 401) this.isInvalid = true;
         else this.isServerIssue = true;
-      });
-
+      }
     },
 
-    registrationPage(){
-      this.$router.push({ name: 'Registration'});
+    registrationPage() {
+      this.$router.push({name: 'Registration'});
     },
 
     forgotPassword() {
-      this.$router.push({ name: 'Forgot'});
+      this.$router.push({name: 'Forgot'});
     },
 
     closeModal() {
@@ -173,8 +122,6 @@ export default {
 </script>
 
 <style scoped>
-
-
 .box {
   display: flex;
   flex-direction: column;
@@ -183,7 +130,7 @@ export default {
 .section {
   display: inline-flex;
   flex-direction: column;
-  
+
 }
 
 .inputbox {
@@ -203,5 +150,4 @@ export default {
   margin: 5px;
   text-align: center;
 }
-
 </style>
