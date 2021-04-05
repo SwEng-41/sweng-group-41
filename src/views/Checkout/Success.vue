4d818d003e353cc0ec975d8a91a264cd97069e45
Ben Vaughan <benovaughan@gmail.com>
2021-04-05 22:18:09 +0100

    Creation of success / cancellation on checkout

---
+++ src/views/Checkout/Success.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <div class="section-inner">
+            
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Success",
+}
+</script>
+
+<style scoped>
+.section-inner {
+    padding: auto;
+}
+</style>
