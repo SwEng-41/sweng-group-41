c9d0165b9e84c921bdaadfd11ff98c15660a671c
swenginator <72451636+swenginator@users.noreply.github.com>
2021-04-06 12:06:33 +0100

    Remove navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link :to="{ name: 'Login' }">Login</router-link>
-    |
-    <router-link :to="{ name: 'Registration' }">Registration</router-link>
-    |
-    <router-link :to="{ name: 'Account' }">Account</router-link>
-    |
-    <router-link :to="{ name: 'Success' }">Success</router-link>
-    |
-    <router-link :to="{ name: 'Cancellation' }">Cancellation</router-link>
-
-  </div>
   <router-view/>
 </template>
 
